bce66c1fdfaec0b2034f2909f11fac3956a34569
AshinaGenichiro <uT_4011@zsp3zamosc.pl>
2026-05-07 10:23:46 +0200

    czesc zadania 1.4

--- src/components/Task1p3.jsx
+++ src/components/Task1p3.jsx
@@ -1,7 +1,7 @@
 import React, { createElement } from 'react';
 
 const headersTable = ["header-1", "header-2", "header-3"]
-
+const paragraph = ["coscos", "coscos123", "socsos12354"]
 
 const Task1p3 = () => {
 const cards = headersTable.map((headingTitle, index) => {
@@ -11,6 +11,16 @@ const cards = headersTable.map((headingTitle, index) => {
             React.createElement(
                 'h2',{ className: 'div-heading' }, 
                 `To jest nagłówek ${headingTitle}`
+            ),
+            React.createElement(
+                'p',
+                {className : 'paragraph-div'},
+                `${paragraph[index]}`
+            ),
+            React.createElement(
+                'a',
+                {className : 'div-a'},
+                'cos'
             )
         );
     });
